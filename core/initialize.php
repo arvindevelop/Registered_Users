8226eec0381b5d94751a1e31b2a144093fc823f9
arvindevelop <aks7631353674@gmail.com>
2021-11-27 16:29:44 +0530

    done

--- core/initialize.php
+++ core/initialize.php
@@ -1,6 +1,6 @@
 <?php
-    defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-    defined('SITE_ROOT') ? null : define('SITE_ROOT', 'https://registered-users.herokuapp.com/');
+    // defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+    // defined('SITE_ROOT') ? null : define('SITE_ROOT', 'https://registered-users.herokuapp.com/');
 
     defined('INC_PATH') ? null : define('INC_PATH',SITE_ROOT.DS.'includes');
     defined('CORE_PATH') ? null : define('CORE_PATH',SITE_ROOT.DS.'core');
