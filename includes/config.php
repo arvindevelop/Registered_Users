304541fe104184349f4ba718961d32ebb3ca32eb
arvindevelop <aks7631353674@gmail.com>
2021-11-27 13:45:11 +0530

    remotely deployed

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,19 @@
 <?php
-    $db_user = 'root';
-    $db_password = '';
-    $db_name = 'users_api';
+    // $db_user = 'root';
+    // $db_password = '';
+    // $db_name = 'users_api';
 
-    $db = new PDO('mysql:host=127.0.0.1;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
+    $db_user = 'mP0ewlSNqi';
+    $db_password = 'KoLhWQe5cI';
+    $db_name = 'mP0ewlSNqi';
+
+    $db = new PDO('mysql:host=remotemysql.com;dbname='.$db_name.';charset=utf8',$db_user,$db_password);
 
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,true);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $con=mysqli_connect("localhost", $db_user, $db_password, $db_name);
+    $con=mysqli_connect("remotemysql.com", $db_user, $db_password, $db_name);
     if(mysqli_connect_errno()){
     echo "Connection Fail".mysqli_connect_error();
 }
