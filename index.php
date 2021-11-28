056f8f990d43ef65f2580687b386e61a1916572b
arvindevelop <aks7631353674@gmail.com>
2021-11-28 08:50:13 +0530

    Updated

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ include_once('includes/config.php');
                 //pagination
                 $no_of_records_per_page = 3;
                 $offset = ($pageno-1) * $no_of_records_per_page;
-                $total_pages_sql = "SELECT COUNT(*) FROM users";
+                //$total_pages_sql = "SELECT COUNT(*) FROM users";
                 $ret1=mysqli_query($con,"select COUNT(*) from users");
                 $total_rows = mysqli_fetch_array($ret1)[0];
                 $total_pages = ceil($total_rows / $no_of_records_per_page);
